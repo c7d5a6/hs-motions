6471046e348f84be055096ea60d96d9c5fd9bd8c
Mikita Kukavenka <nkukovenko@by.byelex.com>
2023-10-13 17:40:32 +0300

    Added some functions

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,4 +5,15 @@ module Lib
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-mainFunc :: CousorPosition -> GoalPosition -> Line -> Answer 
+data Point = Point {col :: Int}
+type Text = String
+type MotionAction = String
+
+printMotions :: Point -> Point -> Text -> String
+printMotions coursor goal text = foldl (<>) "" $ fmap printMotionAction $ findMotions coursor goal text
+
+printMotionAction :: MotionAction -> String
+printMotionAction = id
+
+findMotions :: Point -> Point -> Text -> [MotionAction]
+findMotions _ _ _ = []
