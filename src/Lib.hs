c0006a99ad53b42b3b3b419cd9261c1d25f483ce
c7d5a6 <godinfrog@gmail.com>
2023-10-18 00:58:46 +0300

    fix

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,65 +1,91 @@
 module Lib
-    ( someFunc,
+  ( someFunc,
     findMotions,
     aStar,
-    astar,
+    astar',
     isGoal10,
     lenToGoal,
-    generateNextN
-    ) where
+    generateNextN,
+  )
+where
 
 import Data.TextTypes
+
 -- | I am the 'foo' function's Haddock pre-comment.
+
 {-          +--> some info
             |
             + -}
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
-printMotions :: Point -> Point -> Text -> String -- ^ The answer
+printMotions ::
+  Point ->
+  Point ->
+  Text ->
+  -- | The answer
+  String
 printMotions coursor goal text = foldl (<>) "" (printMotionAction <$> findMotions coursor goal text)
 
 printMotionAction :: MotionAction -> String
 printMotionAction = id
 
 -- | This is findMotions function
-findMotions :: Point -- ^ Initial point
-            -> Point -- ^ Goal Point
-            -> Text -> [MotionAction]
+findMotions ::
+  -- | Initial point
+  Point ->
+  -- | Goal Point
+  Point ->
+  Text ->
+  [MotionAction]
 findMotions _ _ _ = []
 
-aStar :: Point -- ^ Initial point
-      -> Point -- ^ Goal point
-      -> (Point -> Point -> Int) -- ^ h(x) function of cost from point to point
-      -> (Point -> Point -> Int) -- ^ g(x) function of cost to move from point to point
-      -> Text -- ^ Text used for navigation
-      -> (Point -> [Point]) -- ^ neighbours generator
-      -> [MotionAction] -- ^ Route to navigate to goal
+aStar ::
+  -- | Initial point
+  Point ->
+  -- | Goal point
+  Point ->
+  -- | h(x) function of cost from point to point
+  (Point -> Point -> Int) ->
+  -- | g(x) function of cost to move from point to point
+  (Point -> Point -> Int) ->
+  -- | Text used for navigation
+  Text ->
+  -- | neighbours generator
+  (Point -> [Point]) ->
+  -- | Route to navigate to goal
+  [MotionAction]
 aStar _ _ _ _ _ _ = []
 
-astar' :: (Point -> Bool) -- Is point a goal
-       -> (Point -> Int) -- heuristic function
-       -> [(Point, Int)] -- Next to check
-       -> [Point] -- Visited
-       -> ((Point, Int) -> [(Point, Int)]) -- neighbours generator
-       -> Maybe [Point] -- ^ Route to navigate to goal
+astar' ::
+  (Point -> Bool) -> -- Is point a goal
+  (Point -> Int) -> -- heuristic function
+  [(Point, Int)] -> -- Next to check
+  [Point] -> -- Visited
+  ((Point, Int) -> [(Point, Int)]) -> -- neighbours generator
+
+  -- | Route to navigate to goal
+  Maybe [Point]
 astar' _ _ [] _ _ = Nothing
 astar' isGoal h next seen getNext
-    | isGoal p = Just [p]
-    | p `elem` seen = astar' isGoal h nextTail seen getNext
-    | otherwise = fmap (p :) (astar' isGoal h next' (p:seen) getNext)
-    where
-        nextPoint' [x] = x
-        nextPoint' list = foldr1 minPair list
-        nextPoint = nextPoint' next
-        p = fst nextPoint
-        nextTail = removeItem nextPoint next
-        minPair (x1,y1) (x2,y2) = if (h x1 + y1)<(h x2 + y2) then (x1,y1) else (x2,y2)
-        next' = nextTail <> getNext nextPoint
-        removeItem _ []                 = []
-        removeItem x (y:ys) | x == y    = removeItem x ys
-                    | otherwise = y : removeItem x ys
+  | isGoal p = Just [p]
+  | p `elem` seen = astar' isGoal h nextTail seen getNext
+  | otherwise = fmap (p :) (astar' isGoal h next' (p : seen) getNext)
+  where
+    nextPoint' [x] = x
+    nextPoint' list = foldr1 minPair list
+    nextPoint = nextPoint' next
+    p = fst nextPoint
+    nextTail = removeItem nextPoint next
+    minPair (x1, y1) (x2, y2) = if (h x1 + y1) < (h x2 + y2) then (x1, y1) else (x2, y2)
+    next' = nextTail <> getNext nextPoint
+    removeItem _ [] = []
+    removeItem x (y : ys)
+      | x == y = removeItem x ys
+      | otherwise = y : removeItem x ys
 
-generateNextN (p,f) = [(Point {col=col p-1},f+1),(Point {col=col p+1}, f+1)]
+generateNextN (p, f) = [(Point {col = col p - 1}, f + 1), (Point {col = col p + 1}, f + 1)]
+
 isGoal10 p = col p == 10
+
 lenToGoal p = abs $ col p - 10
